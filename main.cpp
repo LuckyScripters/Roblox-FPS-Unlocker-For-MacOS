3335d4ad881e229145aacd3939fd055f396e9c9c
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2024-06-12 17:23:47 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#include <sys/stat.h>
+
+using namespace std;
+
+int fpsCaps = 10000;
+string separator = ", ";
+
+void addClientSettings()
+{
+    string answer;
+    const char* clientSettingsPath = "/Applications/Roblox.app/Contents/MacOS/ClientSettings";
+    const string clientAppSettingsPath = "/Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json";
+    cout << "Do you want to use Vulkan? Answer yes or no." << endl;
+    getline(cin, answer);
+    if (answer == "yes")
+    {
+        int directoryCreationStatus = mkdir(clientSettingsPath, S_IRUSR | S_IWUSR | S_IXUSR);
+        if (directoryCreationStatus == 0)
+        {
+            cout << "File has been created" << endl;
+        }
+        else
+        {
+            cout << "Failed to create file: " << strerror(errno) << endl;
+            exit(-1);
+        }
+        ofstream clientAppSettings(clientAppSettingsPath);
+        clientAppSettings << R"({"DFIntTaskSchedulerTargetFps": )" + to_string(fpsCaps);
+        clientAppSettings << separator;
+        clientAppSettings << R"("FFlagDebugGraphicsDisableMetal": true)";
+        clientAppSettings << separator;
+        clientAppSettings << R"("FFlagDebugGraphicsPreferVulkan": true)";
+        clientAppSettings << "}" << endl;
+        clientAppSettings.close();
+        cout << "Successfully set fpsCaps to " + to_string(fpsCaps) + " with Vulkan mode" << endl;
+    }
+    else if (answer == "no")
+    {
+        int directoryCreationStatus = mkdir(clientSettingsPath, S_IRUSR | S_IWUSR | S_IXUSR);
+        if (directoryCreationStatus == 0)
+        {
+            cout << "File has been created" << endl;
+        }
+        else
+        {
+            cout << "Failed to create file: " << strerror(errno) << endl;
+            exit(-1);
+        }
+        ofstream clientAppSettings(clientAppSettingsPath);
+        clientAppSettings << R"({"DFIntTaskSchedulerTargetFps": )" + to_string(fpsCaps);
+        clientAppSettings << "}" << endl;
+        clientAppSettings.close();
+        cout << "Successfully set fpsCaps to " + to_string(fpsCaps) << endl;
+    }
+    else
+    {
+        cout << "Invalid answer" << endl;
+        exit(-1);
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    string answer;
+    cout << "Do you want to create ClientSettings? Answer create" << endl;
+    getline(cin, answer);
+    if (answer == "create")
+    {
+        addClientSettings();
+    }
+    else
+    {
+        cout << "Invalid Answer" << endl;
+    }
+    return 0;
+}
